b6e6e73e2f0291d75a0931d7c3d2efe822d5ac48
Joseph Vano <joseph.vano@gmail.com>
2022-11-12 12:36:33 -0500

    feat: add video route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-import express from 'express'
+import express    from 'express';
+import path       from 'path';
+import fsPromises from "fs/promises";
+import fs         from "fs";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port        = process.env.PORT || 3000;
+const PUBLIC_PATH = path.join(__dirname, "../public");
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
+})
+
+app.get("/video", async (req, res) => {
+  const videoPath = path.join(PUBLIC_PATH, "SampleVideo_1280x720_1mb.mp4");
+  const file      = await fsPromises.stat(videoPath)
+
+  res.writeHead(200, {
+    "Content-Length": file.size,
+    "Content-Type"  : "video/mp4"
+  });
+
+  fs.createReadStream(videoPath).pipe(res);
 })
 
 app.listen(port, () => {
