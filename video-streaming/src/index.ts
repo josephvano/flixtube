6a72f4957473989022c39139e454a014d2739647
Joseph Vano <joseph.vano@gmail.com>
2022-11-19 23:07:21 -0500

    feat(streaming): use storage service to load video and stream

--- video-streaming/src/index.ts
+++ video-streaming/src/index.ts
@@ -1,35 +1,49 @@
-import express    from 'express';
-import path       from 'path';
-import fsPromises from "fs/promises";
-import fs         from "fs";
+import express, {
+  Request,
+  Response
+}                              from 'express';
+import http, {IncomingMessage} from "http";
+import debug                   from "debug";
 
 const app = express();
+const log = debug("video-streaming:api")
 
-const port        = process.env.PORT || 3000;
-const PUBLIC_PATH = path.join(__dirname, "../public");
+const port         = process.env.PORT || 4000;
+const STORAGE_HOST = process.env.STORAGE_HOST || "localhost";
+const STORAGE_PORT = process.env.STORAGE_PORT || 4001;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-})
+app.get("/healthz", (req: Request, res: Response) => {
+  log("health check");
 
-app.get("/healthz", (req, res) => {
   res.send({
     ok: true
   })
 });
 
-app.get("/video", async (req, res) => {
-  const videoPath = path.join(PUBLIC_PATH, "SampleVideo_1280x720_1mb.mp4");
-  const file      = await fsPromises.stat(videoPath)
+app.get("/videos", async (req: Request, res: Response) => {
+  const videoPath = "SampleVideo_1280x720_1mb.mp4";
 
-  res.writeHead(200, {
-    "Content-Length": file.size,
-    "Content-Type"  : "video/mp4"
-  });
+  log(`fetching ${videoPath} at ${STORAGE_HOST}:${STORAGE_PORT}`)
 
-  fs.createReadStream(videoPath).pipe(res);
+  const forwardRequest = http.request({
+      host   : STORAGE_HOST,
+      port   : STORAGE_PORT,
+      path   : `/videos/?path=${videoPath}`,
+      method : "GET",
+      headers: req.headers
+    },
+    (forwardResponse: IncomingMessage) => {
+      log('callback')
+      if (forwardResponse.statusCode) {
+        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+      }
+
+      forwardResponse.pipe(res)
+    });
+
+  req.pipe(forwardRequest);
 })
 
 app.listen(port, () => {
-  console.log(`Starting server on port ${port}`);
+  log(`starting server on port ${port}`);
 })
