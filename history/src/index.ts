1c226eaece03e2d9560f2a02ac546155bcc7af92
Joseph Vano <joseph.vano@gmail.com>
2022-11-20 16:18:28 -0500

    wip: add history service

---
+++ history/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import debug   from "debug";
+
+const log = debug("history:api")
+
+const main = async () => {
+  const port = process.env.PORT || 4003;
+
+  const app = express();
+
+  app.get("/healthz", (_, res) => {
+    log("healthz check");
+
+    res.json({
+      ok: true
+    });
+  })
+
+  app.listen(port, () => {
+    log(`starting history service listening on ${port}`);
+  });
+}
+
+main().catch(err => {
+  log(`error in history service`)
+  log(err);
+});
