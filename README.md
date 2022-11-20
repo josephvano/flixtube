99b242590191506719095787ff695dbd64d5d2b9
Joseph Vano <joseph.vano@gmail.com>
2022-11-20 00:09:35 -0500

    feat(db): add mongodb setup and call into collection
    * add mongodb
    * connect to instance via docker-compose
    * fetch path from collection(videos)
    * add docs on env vars

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ Following book Bootstrapping Microservices with Docker, Kubernetes and Terraform
 
 ## Getting Started
 
+### Configuration
+
+Set the following environment variables locally
+
+| Variable | Description | Value |
+| ------------ | --------| -----------|
+| DB_USER | MongoDB username | `root` |
+| DB_PASSWORD | MongoDB password | `null` |
+| STORAGE_KEY | MinIO username | `admin` |
+| STORAGE_SECRET | MinIO password | `null` |
+
+### Running
+
 Modify the `docker-compose.yaml`  with desired credentials for minio bucket storage.
 
 1. Start services
--- video-streaming/src/index.ts
+++ video-streaming/src/index.ts
@@ -4,6 +4,7 @@ import express, {
 }                              from 'express';
 import http, {IncomingMessage} from "http";
 import debug                   from "debug";
+import * as mongodb            from "mongodb";
 
 const app = express();
 const log = debug("video-streaming:api")
@@ -11,39 +12,81 @@ const log = debug("video-streaming:api")
 const port         = process.env.PORT || 4000;
 const STORAGE_HOST = process.env.STORAGE_HOST || "localhost";
 const STORAGE_PORT = process.env.STORAGE_PORT || 4001;
+const DB_HOST      = process.env.DB_HOST || "mongodb://localhost:4002";
+const DB_NAME      = process.env.DB_NAME || "video-streaming";
 
-app.get("/healthz", (req: Request, res: Response) => {
-  log("health check");
+const main = async () => {
+  log(`connecting to ${DB_HOST}`)
+  const client     = await mongodb.MongoClient.connect(DB_HOST);
+  const db         = client.db(DB_NAME);
+  const collection = db.collection("videos");
+
+  app.get("/healthz", (req: Request, res: Response) => {
+    log("health check");
+
+    res.send({
+      ok: true
+    })
+  });
+
+  app.get("/videos", async (req: Request, res: Response) => {
+    const id = new mongodb.ObjectId(req.query["id"] as string)
+
+    log(`finding ${id}`);
+
+    const record = await collection.findOne({_id: id});
+
+    if (!record) {
+      sendError(res, new Error(`Could not find record ${id}`), 404);
+      return;
+    }
+
+    const videoPath = record.path;
+
+    if(!videoPath){
+      sendError(res, new Error(`Could not find video path for ${id}`), 404);
+      return;
+    }
+
+    log(`found ${videoPath}`);
+
+    log(`fetching ${videoPath} at ${STORAGE_HOST}:${STORAGE_PORT}`)
+
+    const forwardRequest = http.request({
+        host   : STORAGE_HOST,
+        port   : STORAGE_PORT,
+        path   : `/videos/?path=${videoPath}`,
+        method : "GET",
+        headers: req.headers
+      },
+      (forwardResponse: IncomingMessage) => {
+        log('callback')
+        if (forwardResponse.statusCode) {
+          res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+        }
+
+        forwardResponse.pipe(res)
+      });
+
+    req.pipe(forwardRequest);
+  })
+
+  app.listen(port, () => {
+    log(`starting server on port ${port}`);
+  })
+}
+
+function sendError(res: Response, error: Error, code: number = 500) {
+  log(error);
+
+  res.status(code);
 
   res.send({
-    ok: true
+    ok     : false,
+    message: error.message
   })
-});
+}
 
-app.get("/videos", async (req: Request, res: Response) => {
-  const videoPath = "SampleVideo_1280x720_1mb.mp4";
-
-  log(`fetching ${videoPath} at ${STORAGE_HOST}:${STORAGE_PORT}`)
-
-  const forwardRequest = http.request({
-      host   : STORAGE_HOST,
-      port   : STORAGE_PORT,
-      path   : `/videos/?path=${videoPath}`,
-      method : "GET",
-      headers: req.headers
-    },
-    (forwardResponse: IncomingMessage) => {
-      log('callback')
-      if (forwardResponse.statusCode) {
-        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-      }
-
-      forwardResponse.pipe(res)
-    });
-
-  req.pipe(forwardRequest);
-})
-
-app.listen(port, () => {
-  log(`starting server on port ${port}`);
+main().catch(err => {
+  log(err);
 })
